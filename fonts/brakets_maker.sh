041648faa9bf78b95d0cf078ee9d892cdb13c72c
WFTurnip <95839021+WFTurnip@users.noreply.github.com>
2025-05-01 22:04:20 +0900

    update svg & shell file

--- fonts/brakets_maker.sh
+++ fonts/brakets_maker.sh
@@ -1,17 +1,17 @@
-# cp polwgo.svg polwgo/uni003C.svg
-# cp polwgo.svg polwgo/uni003E.svg
-# cp polwgo.svg polwgo/uni0028.svg
-# cp polwgo.svg polwgo/uni0029.svg
-# cp polwgo.svg polwgo/uni005B.svg
-# cp polwgo.svg polwgo/uni005D.svg
-# cp polwgo.svg polwgo/uni007B.svg
-# cp polwgo.svg polwgo/uni007D.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni003C.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni003E.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0028.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0029.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni005B.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni005D.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni007B.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni007D.svg
 
-# open -a /Applications/Visual\ Studio\ Code.app polwgo/uni003C.svg
-# open -a /Applications/Visual\ Studio\ Code.app polwgo/uni003E.svg
-# open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0028.svg
-# open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0029.svg
-# open -a /Applications/Visual\ Studio\ Code.app polwgo/uni005B.svg
-# open -a /Applications/Visual\ Studio\ Code.app polwgo/uni005D.svg
-# open -a /Applications/Visual\ Studio\ Code.app polwgo/uni007B.svg
-# open -a /Applications/Visual\ Studio\ Code.app polwgo/uni007D.svg
+open polwgo/uni003C.svg
+open polwgo/uni003E.svg
+open polwgo/uni0028.svg
+open polwgo/uni0029.svg
+open polwgo/uni005B.svg
+open polwgo/uni005D.svg
+open polwgo/uni007B.svg
+open polwgo/uni007D.svg
