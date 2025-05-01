89e06b705f1f89412df73730858c50c1ddf8d7f7
WFTurnip <95839021+WFTurnip@users.noreply.github.com>
2025-05-01 19:20:39 +0900

    update

--- fonts/consonants_concatenator.sh
+++ fonts/consonants_concatenator.sh
@@ -1,23 +1,23 @@
-cat kodito/uni004b.svg kodito/uni005c.svg >kodito/uni0047.svg
-cat kodito/uni0054.svg kodito/uni005c.svg >kodito/uni0044.svg
-cat kodito/uni0053.svg kodito/uni005c.svg >kodito/uni005a.svg
-cat kodito/uni0051.svg kodito/uni005c.svg >kodito/uni0043.svg
-cat kodito/uni0052.svg kodito/uni005c.svg >kodito/uni004c.svg
-cat kodito/uni0050.svg kodito/uni005c.svg >kodito/uni0042.svg
-cat kodito/uni0048.svg kodito/uni005c.svg >kodito/uni0058.svg
-cat kodito/uni0046.svg kodito/uni005c.svg >kodito/uni0056.svg
-cat kodito/uni004d.svg kodito/uni005c.svg >kodito/uni004e.svg
-cat kodito/uni0053.svg kodito/uni005c.svg kodito/uni0030.svg >kodito/uni004a.svg
-cat kodito/uni0051.svg kodito/uni005c.svg kodito/uni0030.svg >kodito/uni0059.svg
+cat polwgo/uni004b.svg polwgo/uni005c.svg >polwgo/uni0047.svg
+cat polwgo/uni0054.svg polwgo/uni005c.svg >polwgo/uni0044.svg
+cat polwgo/uni0053.svg polwgo/uni005c.svg >polwgo/uni005a.svg
+cat polwgo/uni0051.svg polwgo/uni005c.svg >polwgo/uni0043.svg
+cat polwgo/uni0052.svg polwgo/uni005c.svg >polwgo/uni004c.svg
+cat polwgo/uni0050.svg polwgo/uni005c.svg >polwgo/uni0042.svg
+cat polwgo/uni0048.svg polwgo/uni005c.svg >polwgo/uni0058.svg
+cat polwgo/uni0046.svg polwgo/uni005c.svg >polwgo/uni0056.svg
+cat polwgo/uni004d.svg polwgo/uni005c.svg >polwgo/uni004e.svg
+cat polwgo/uni0053.svg polwgo/uni005c.svg polwgo/uni0030.svg >polwgo/uni004a.svg
+cat polwgo/uni0051.svg polwgo/uni005c.svg polwgo/uni0030.svg >polwgo/uni0059.svg
 
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni0047.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni0044.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni005a.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni0043.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni004c.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni0042.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni0058.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni0056.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni004e.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni004a.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni0059.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0047.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0044.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni005a.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0043.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni004c.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0042.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0058.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0056.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni004e.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni004a.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0059.svg
--- fonts/consonants_maker.sh
+++ fonts/consonants_maker.sh
@@ -1,29 +1,19 @@
-cat kodito.svg >>kodito/uni004b.svg
-cat kodito.svg >>kodito/uni0054.svg
-cat kodito.svg >>kodito/uni0053.svg
-cat kodito.svg >>kodito/uni0051.svg
-cat kodito.svg >>kodito/uni0052.svg
-cat kodito.svg >>kodito/uni0050.svg
-cat kodito.svg >>kodito/uni0048.svg
-cat kodito.svg >>kodito/uni0046.svg
-cat kodito.svg >>kodito/uni004d.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni004b.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0054.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0053.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0051.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0052.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0050.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0048.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0046.svg
+open -a /Applications/Visual\ Studio\ Code.app polwgo/uni004d.svg
 
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni004b.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni0054.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni0053.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni0051.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni0052.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni0050.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni0048.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni0046.svg
-open -a /Applications/Visual\ Studio\ Code.app kodito/uni004d.svg
-
-# svgo kodito/uni004b.svg
-# svgo kodito/uni0054.svg
-# svgo kodito/uni0053.svg
-# svgo kodito/uni0051.svg
-# svgo kodito/uni0052.svg
-# svgo kodito/uni0050.svg
-# svgo kodito/uni0048.svg
-# svgo kodito/uni0046.svg
-# svgo kodito/uni004d.svg
+open polwgo/uni004b.svg
+open polwgo/uni0054.svg
+open polwgo/uni0053.svg
+open polwgo/uni0051.svg
+open polwgo/uni0052.svg
+open polwgo/uni0050.svg
+open polwgo/uni0048.svg
+open polwgo/uni0046.svg
+open polwgo/uni004d.svg
