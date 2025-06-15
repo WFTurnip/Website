b1e5d7085d51e01c238ce79156d30d3b5e941001
WFTurnip <95839021+WFTurnip@users.noreply.github.com>
2025-06-16 01:00:35 +0900

    update

---
+++ roundterm/basic_latin.sh
@@ -0,0 +1,188 @@
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0021.svg
+# !
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0022.svg
+# "
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0023.svg
+# #
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0024.svg
+# $
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0025.svg
+# %
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0026.svg
+# &
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0027.svg
+# '
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0028.svg
+# (
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0029.svg
+# )
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni002a.svg
+# *
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni002b.svg
+# +
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni002c.svg
+# ,
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni002d.svg
+# -
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni002e.svg
+# .
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni002f.svg
+# /
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0030.svg
+# 0
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0031.svg
+# 1
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0032.svg
+# 2
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0033.svg
+# 3
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0034.svg
+# 4
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0035.svg
+# 5
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0036.svg
+# 6
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0037.svg
+# 7
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0038.svg
+# 8
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0039.svg
+# 9
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni003a.svg
+# :
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni003b.svg
+# ;
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni003c.svg
+# <
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni003d.svg
+# =
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni003e.svg
+# >
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni003f.svg
+# ?
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0040.svg
+# @
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0041.svg
+# A
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0042.svg
+# B
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0043.svg
+# C
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0044.svg
+# D
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0045.svg
+# E
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0046.svg
+# F
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0047.svg
+# G
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0048.svg
+# H
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0049.svg
+# I
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni004a.svg
+# J
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni004b.svg
+# K
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni004c.svg
+# L
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni004d.svg
+# M
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni004e.svg
+# N
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni004f.svg
+# O
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0050.svg
+# P
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0051.svg
+# Q
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0052.svg
+# R
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0053.svg
+# S
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0054.svg
+# T
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0055.svg
+# U
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0056.svg
+# V
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0057.svg
+# W
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0058.svg
+# X
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0059.svg
+# Y
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni005a.svg
+# Z
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni005b.svg
+# [
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni005c.svg
+# \
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni005d.svg
+# ]
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni005e.svg
+# ^
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni005f.svg
+# _
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0060.svg
+# `
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0061.svg
+# a
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0062.svg
+# b
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0063.svg
+# c
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0064.svg
+# d
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0065.svg
+# e
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0066.svg
+# f
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0067.svg
+# g
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0068.svg
+# h
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0069.svg
+# i
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni006a.svg
+# j
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni006b.svg
+# k
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni006c.svg
+# l
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni006d.svg
+# m
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni006e.svg
+# n
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni006f.svg
+# o
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0070.svg
+# p
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0071.svg
+# q
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0072.svg
+# r
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0073.svg
+# s
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0074.svg
+# t
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0075.svg
+# u
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0076.svg
+# v
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0077.svg
+# w
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0078.svg
+# x
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni0079.svg
+# y
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni007a.svg
+# z
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni007b.svg
+# {
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni007c.svg
+# |
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni007d.svg
+# }
+open -a /Applications/Visual\ Studio\ Code.app basic_latin/uni007e.svg
+# ~
