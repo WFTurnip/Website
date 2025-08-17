e075d94b2875345f50062f652aeb866f79e53d4c
WFTurnip <95839021+WFTurnip@users.noreply.github.com>
2025-08-17 18:44:13 +0900

    change

--- fonts/consonants_concatenator.sh
+++ fonts/consonants_concatenator.sh
@@ -1,23 +1,23 @@
-cat polwgo/uni004b.svg polwgo/uni005c.svg >polwgo/uni0047.svg
-cat polwgo/uni0054.svg polwgo/uni005c.svg >polwgo/uni0044.svg
-cat polwgo/uni0053.svg polwgo/uni005c.svg >polwgo/uni005a.svg
-cat polwgo/uni0051.svg polwgo/uni005c.svg >polwgo/uni0043.svg
-cat polwgo/uni0052.svg polwgo/uni005c.svg >polwgo/uni004c.svg
-cat polwgo/uni0050.svg polwgo/uni005c.svg >polwgo/uni0042.svg
-cat polwgo/uni0048.svg polwgo/uni005c.svg >polwgo/uni0058.svg
-cat polwgo/uni0046.svg polwgo/uni005c.svg >polwgo/uni0056.svg
-cat polwgo/uni004d.svg polwgo/uni005c.svg >polwgo/uni004e.svg
-cat polwgo/uni0053.svg polwgo/uni005c.svg polwgo/uni0030.svg >polwgo/uni004a.svg
-cat polwgo/uni0051.svg polwgo/uni005c.svg polwgo/uni0030.svg >polwgo/uni0059.svg
+cat piswpi/uni004b.svg piswpi/uni005c.svg >piswpi/uni0047.svg
+cat piswpi/uni0054.svg piswpi/uni005c.svg >piswpi/uni0044.svg
+cat piswpi/uni0053.svg piswpi/uni005c.svg >piswpi/uni005a.svg
+cat piswpi/uni0051.svg piswpi/uni005c.svg >piswpi/uni0043.svg
+cat piswpi/uni0052.svg piswpi/uni005c.svg >piswpi/uni004c.svg
+cat piswpi/uni0050.svg piswpi/uni005c.svg >piswpi/uni0042.svg
+cat piswpi/uni0048.svg piswpi/uni005c.svg >piswpi/uni0058.svg
+cat piswpi/uni0046.svg piswpi/uni005c.svg >piswpi/uni0056.svg
+cat piswpi/uni004d.svg piswpi/uni005c.svg >piswpi/uni004e.svg
+cat piswpi/uni0053.svg piswpi/uni005c.svg piswpi/uni0030.svg >piswpi/uni004a.svg
+cat piswpi/uni0051.svg piswpi/uni005c.svg piswpi/uni0030.svg >piswpi/uni0059.svg
 
-open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0047.svg
-open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0044.svg
-open -a /Applications/Visual\ Studio\ Code.app polwgo/uni005a.svg
-open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0043.svg
-open -a /Applications/Visual\ Studio\ Code.app polwgo/uni004c.svg
-open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0042.svg
-open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0058.svg
-open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0056.svg
-open -a /Applications/Visual\ Studio\ Code.app polwgo/uni004e.svg
-open -a /Applications/Visual\ Studio\ Code.app polwgo/uni004a.svg
-open -a /Applications/Visual\ Studio\ Code.app polwgo/uni0059.svg
+open -a /Applications/Visual\ Studio\ Code.app piswpi/uni0047.svg
+open -a /Applications/Visual\ Studio\ Code.app piswpi/uni0044.svg
+open -a /Applications/Visual\ Studio\ Code.app piswpi/uni005a.svg
+open -a /Applications/Visual\ Studio\ Code.app piswpi/uni0043.svg
+open -a /Applications/Visual\ Studio\ Code.app piswpi/uni004c.svg
+open -a /Applications/Visual\ Studio\ Code.app piswpi/uni0042.svg
+open -a /Applications/Visual\ Studio\ Code.app piswpi/uni0058.svg
+open -a /Applications/Visual\ Studio\ Code.app piswpi/uni0056.svg
+open -a /Applications/Visual\ Studio\ Code.app piswpi/uni004e.svg
+open -a /Applications/Visual\ Studio\ Code.app piswpi/uni004a.svg
+open -a /Applications/Visual\ Studio\ Code.app piswpi/uni0059.svg
