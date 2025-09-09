bdaae63e7551b6ef93a3c2984510bea451b1540f
WFTurnip <95839021+WFTurnip@users.noreply.github.com>
2025-09-09 14:06:40 +0900

    update

---
+++ contact.js
@@ -0,0 +1,6 @@
+const user = "tomohiro.kabura";
+const domain = "icloud.com";
+const email = user + "@" + domain;
+
+const link = document.getElementById("emailLink");
+link.href = "mailto:" + email;
