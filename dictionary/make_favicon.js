c9ef48660707ad75b82a4a1ec882c51612e8c07e
WFTurnip <tomohiro.kabura@icloud.com>
2025-09-23 23:57:51 +0900

    updated

---
+++ dictionary/make_favicon.js
@@ -0,0 +1,14 @@
+const fs = require("fs").promises;
+
+async function make() {
+    let directory = path.join("favicon_index");
+    try {
+        await fs.mkdir(directory, { recursive: true });
+        console.log("ディレクトリ" + directory + "を作成しました。");
+    } catch (error) {
+        console.error("ディレクトリ" + directory + "を作成できませんでした。");
+    }
+    console.log("ディレクトリ" + directory + "の内部データを生成完了");
+}
+
+make();
