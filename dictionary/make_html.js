7f3f74996d29677fb880ab3f25f10edd9d670ed3
WFTurnip <tomohiro.kabura@icloud.com>
2025-09-23 16:53:28 +0900

    update

--- dictionary/make_html.js
+++ dictionary/make_html.js
@@ -271,21 +271,12 @@ async function generateRoot(i, j, k) {
     body.appendChild(h1);
 
     let p1 = document.createElement("p");
-    let span = document.createElement("span");
-    span.className = "piswpi";
-    p1.appendChild(document.createTextNode("このページは語根"));
-    span.textContent = consonants_array[i] + consonants_array[j] + consonants_array[k];
-    p1.appendChild(span);
-    p1.appendChild(document.createTextNode("に関する情報を表示します。"));
-    body.appendChild(p1);
-
-    let p2 = document.createElement("p");
     let strong = document.createElement("strong");
-    p2.appendChild(document.createTextNode("この語根は"));
+    p1.appendChild(document.createTextNode("この語根は"));
     strong.textContent = consonants_means_array[i] + consonants_means_array[j] + consonants_means_array[k];
-    p2.appendChild(strong);
-    p2.appendChild(document.createTextNode("の概念を持つ"));
-    body.appendChild(p2);
+    p1.appendChild(strong);
+    p1.appendChild(document.createTextNode("の概念を持つ"));
+    body.appendChild(p1);
 
     let main = document.createElement("main");
 
