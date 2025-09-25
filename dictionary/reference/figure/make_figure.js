cf62170ec44509c19fd5d75903cb2163c84f77d2
WFTurnip <tomohiro.kabura@icloud.com>
2025-09-25 19:33:13 +0900

    update

--- dictionary/reference/figure/make_figure.js
+++ dictionary/reference/figure/make_figure.js
@@ -3,15 +3,74 @@ const path = require("path");
 const { JSDOM } = require("jsdom");
 const beautify = require("js-beautify").html;
 
-generateArticleConjugationFigure();
-generateVerbConjugationFigure();
-generateAdjectiveConjugationFigure();
-generateParticleConjugationFigure();
-generateAdverbConjugationFigure();
-generateNounConjugationFigure();
+async function generateArticleConjugationFigure() {
+    let filename = path.join("");
 
+    try {
+        await fs.writeFile(filename, "");
+        console.log("ファイル" + filename + "を作成しました。");
+    } catch (error) {
+        console.log("ファイル" + filename + "を作成できませんでした。");
+    }
+}
+
+async function generateVerbConjugationFigure() {
+    let filename = path.join("");
+
+    try {
+        await fs.writeFile(filename, "");
+        console.log("ファイル" + filename + "を作成しました。");
+    } catch (error) {
+        console.log("ファイル" + filename + "を作成できませんでした。");
+    }
+}
+
+async function generateAdjectiveConjugationFigure() {
+    let filename = path.join("");
+
+    try {
+        await fs.writeFile(filename, "");
+        console.log("ファイル" + filename + "を作成しました。");
+    } catch (error) {
+        console.log("ファイル" + filename + "を作成できませんでした。");
+    }
+}
+
+async function generateParticleConjugationFigure() {
+    let filename = path.join("");
+
+    try {
+        await fs.writeFile(filename, "");
+        console.log("ファイル" + filename + "を作成しました。");
+    } catch (error) {
+        console.log("ファイル" + filename + "を作成できませんでした。");
+    }
+}
+
+async function generateAdverbConjugationFigure() {
+    let filename = path.join("");
+
+    try {
+        await fs.writeFile(filename, "");
+        console.log("ファイル" + filename + "を作成しました。");
+    } catch (error) {
+        console.log("ファイル" + filename + "を作成できませんでした。");
+    }
+}
+
+async function generateNounConjugationFigure() {
+    let filename = path.join("");
+
+    try {
+        await fs.writeFile(filename, "");
+        console.log("ファイル" + filename + "を作成しました。");
+    } catch (error) {
+        console.log("ファイル" + filename + "を作成できませんでした。");
+    }
+}
 
 async function make() {
+    console.log("図表生成開始");
     generateArticleConjugationFigure();
     generateVerbConjugationFigure();
     generateAdjectiveConjugationFigure();
