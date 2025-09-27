ba54aa43c8d2095098a0e6fe57166bca0ce5189c
WFTurnip <tomohiro.kabura@icloud.com>
2025-09-27 21:57:11 +0900

    update

--- keyboard.js
+++ keyboard.js
@@ -1,1 +1,16 @@
-console.log("ささみ");
+const keyboard = document.getElementById("keyboard");
+let shiftPressed = false;
+
+document.addEventListener("keydown", e => {
+    if (e.key === "Shift") {
+        shiftPressed = true;
+        keyboard.classList.add("shift-active");
+    }
+});
+
+document.addEventListener("keyup", e => {
+    if (e.key === "Shift") {
+        shiftPressed = false;
+        keyboard.classList.remove("shift-active");
+    }
+});
