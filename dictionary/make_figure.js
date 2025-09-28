159abb1130b799d2259be9491fe1ff120157162f
WFTurnip <tomohiro.kabura@icloud.com>
2025-09-29 05:53:44 +0900

    Update make_figure.js

---
+++ dictionary/make_figure.js
@@ -0,0 +1,208 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { JSDOM } = require("jsdom");
+const beautify = require("js-beautify").html;
+
+async function generateArticleConjugationFigure() {
+    let filename = path.join("article_conjugation_figure.svg");
+    const svgns = "http://www.w3.org/2000/svg";
+
+    const dom = new JSDOM();
+    const document = dom.window.document;
+
+    let svg = document.createElementNS(svgns, "svg");
+    svg.setAttribute("width", 100);
+    svg.setAttribute("height", 100);
+
+    let circle = document.createElementNS(svgns, "circle");
+    circle.setAttribute("cx", 50);
+    circle.setAttribute("cy", 50);
+    circle.setAttribute("r", 50);
+    svg.appendChild(circle);
+
+    let serializer = new dom.window.XMLSerializer();
+    let svgString = serializer.serializeToString(svg);
+
+    let svgContent = '<?xml-stylesheet href="../../style/figure.css" type="text/css"?>' + "\n" + svgString;
+
+    svgContent = beautify(svgContent, { indent_size: 4 });
+
+    try {
+        await fs.writeFile(filename, svgContent);
+        console.log("ファイル" + filename + "を作成しました。");
+    } catch (error) {
+        console.log("ファイル" + filename + "を作成できませんでした。");
+    }
+}
+
+async function generateVerbConjugationFigure() {
+    let filename = path.join("verb_conjugation_figure.svg");
+    const svgns = "http://www.w3.org/2000/svg";
+
+    const dom = new JSDOM();
+    const document = dom.window.document;
+
+    let svg = document.createElementNS(svgns, "svg");
+    svg.setAttribute("width", 100);
+    svg.setAttribute("height", 100);
+
+    let circle = document.createElementNS(svgns, "circle");
+    circle.setAttribute("cx", 50);
+    circle.setAttribute("cy", 50);
+    circle.setAttribute("r", 50);
+    svg.appendChild(circle);
+
+    let serializer = new dom.window.XMLSerializer();
+    let svgString = serializer.serializeToString(svg);
+
+    let svgContent = '<?xml-stylesheet href="../../style/figure.css" type="text/css"?>' + "\n" + svgString;
+
+    svgContent = beautify(svgContent, { indent_size: 4 });
+
+    try {
+        await fs.writeFile(filename, svgContent);
+        console.log("ファイル" + filename + "を作成しました。");
+    } catch (error) {
+        console.log("ファイル" + filename + "を作成できませんでした。");
+    }
+}
+
+async function generateAdjectiveConjugationFigure() {
+    let filename = path.join("adjective_conjugation_figure.svg");
+    const svgns = "http://www.w3.org/2000/svg";
+
+    const dom = new JSDOM();
+    const document = dom.window.document;
+
+    let svg = document.createElementNS(svgns, "svg");
+    svg.setAttribute("width", 100);
+    svg.setAttribute("height", 100);
+
+    let circle = document.createElementNS(svgns, "circle");
+    circle.setAttribute("cx", 50);
+    circle.setAttribute("cy", 50);
+    circle.setAttribute("r", 50);
+    svg.appendChild(circle);
+
+    let serializer = new dom.window.XMLSerializer();
+    let svgString = serializer.serializeToString(svg);
+
+    let svgContent = '<?xml-stylesheet href="../../style/figure.css" type="text/css"?>' + "\n" + svgString;
+
+    svgContent = beautify(svgContent, { indent_size: 4 });
+
+    try {
+        await fs.writeFile(filename, svgContent);
+        console.log("ファイル" + filename + "を作成しました。");
+    } catch (error) {
+        console.log("ファイル" + filename + "を作成できませんでした。");
+    }
+}
+
+async function generateParticleConjugationFigure() {
+    let filename = path.join("particle_conjugation_figure.svg");
+    const svgns = "http://www.w3.org/2000/svg";
+
+    const dom = new JSDOM();
+    const document = dom.window.document;
+
+    let svg = document.createElementNS(svgns, "svg");
+    svg.setAttribute("width", 100);
+    svg.setAttribute("height", 100);
+
+    let circle = document.createElementNS(svgns, "circle");
+    circle.setAttribute("cx", 50);
+    circle.setAttribute("cy", 50);
+    circle.setAttribute("r", 50);
+    svg.appendChild(circle);
+
+    let serializer = new dom.window.XMLSerializer();
+    let svgString = serializer.serializeToString(svg);
+
+    let svgContent = '<?xml-stylesheet href="../../style/figure.css" type="text/css"?>' + "\n" + svgString;
+
+    svgContent = beautify(svgContent, { indent_size: 4 });
+
+    try {
+        await fs.writeFile(filename, svgContent);
+        console.log("ファイル" + filename + "を作成しました。");
+    } catch (error) {
+        console.log("ファイル" + filename + "を作成できませんでした。");
+    }
+}
+
+async function generateAdverbConjugationFigure() {
+    let filename = path.join("adverb_conjugation_figure.svg");
+    const svgns = "http://www.w3.org/2000/svg";
+
+    const dom = new JSDOM();
+    const document = dom.window.document;
+
+    let svg = document.createElementNS(svgns, "svg");
+    svg.setAttribute("width", 100);
+    svg.setAttribute("height", 100);
+
+    let circle = document.createElementNS(svgns, "circle");
+    circle.setAttribute("cx", 50);
+    circle.setAttribute("cy", 50);
+    circle.setAttribute("r", 50);
+    svg.appendChild(circle);
+
+    let serializer = new dom.window.XMLSerializer();
+    let svgString = serializer.serializeToString(svg);
+
+    let svgContent = '<?xml-stylesheet href="../../style/figure.css" type="text/css"?>' + "\n" + svgString;
+    svgContent = beautify(svgContent, { indent_size: 4 });
+
+    try {
+        await fs.writeFile(filename, svgContent);
+        console.log("ファイル" + filename + "を作成しました。");
+    } catch (error) {
+        console.log("ファイル" + filename + "を作成できませんでした。");
+    }
+}
+
+async function generateNounConjugationFigure() {
+    let filename = path.join("noun_conjugation_figure.svg");
+    const svgns = "http://www.w3.org/2000/svg";
+
+    const dom = new JSDOM();
+    const document = dom.window.document;
+
+    let svg = document.createElementNS(svgns, "svg");
+    svg.setAttribute("width", 100);
+    svg.setAttribute("height", 100);
+
+    let circle = document.createElementNS(svgns, "circle");
+    circle.setAttribute("cx", 50);
+    circle.setAttribute("cy", 50);
+    circle.setAttribute("r", 50);
+    svg.appendChild(circle);
+
+    let serializer = new dom.window.XMLSerializer();
+    let svgString = serializer.serializeToString(svg);
+
+    let svgContent = '<?xml-stylesheet href="../../style/figure.css" type="text/css"?>' + "\n" + svgString;
+
+    svgContent = beautify(svgContent, { indent_size: 4 });
+
+    try {
+        await fs.writeFile(filename, svgContent);
+        console.log("ファイル" + filename + "を作成しました。");
+    } catch (error) {
+        console.log("ファイル" + filename + "を作成できませんでした。");
+    }
+}
+
+async function make() {
+    console.log("図表生成開始");
+    generateArticleConjugationFigure();
+    generateVerbConjugationFigure();
+    generateAdjectiveConjugationFigure();
+    generateParticleConjugationFigure();
+    generateAdverbConjugationFigure();
+    generateNounConjugationFigure();
+    console.log("図表生成完了");
+}
+
+make();
