2714079170c8c0baa9196afdea98b183ad72f33c
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-01 16:00:17 +0900

    update

--- dictionary/reference/reference.js
+++ dictionary/reference/reference.js
@@ -1,3 +1,11 @@
-let random = Math.random();
-
-console.log("めっちゃ面白そうだにゃ。" + random);
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", event => {
+        const targetId = link.dataset.target;
+        if (targetId) {
+            const details = document.getElementById(targetId);
+            if (details) {
+                details.open = true;
+            }
+        }
+    });
+});
