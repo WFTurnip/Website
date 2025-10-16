b980e3adc1d236e5ca362f89970311cafbe297bc
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-16 20:08:10 +0900

    update

--- fonts/vowels_concatenator.sh
+++ fonts/vowels_concatenator.sh
@@ -1,5 +1,9 @@
 cat zosokw/uni0049.svg zosokw/uni0045.svg >zosokw/uni0041.svg
 cat zosokw/uni0057.svg zosokw/uni0055.svg >zosokw/uni004F.svg
 
-svgo zosokw/uni0041.svg
-svgo zosokw/uni004F.svg
+open zosokw/uni0049.svg
+open zosokw/uni0045.svg
+open zosokw/uni0041.svg
+open zosokw/uni0057.svg
+open zosokw/uni0055.svg
+open zosokw/uni004F.svg
