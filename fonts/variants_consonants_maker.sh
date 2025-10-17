d40473a963c10461e416033e69a2e30ab63f21fb
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-17 16:13:15 +0900

    update

--- fonts/variants_consonants_maker.sh
+++ fonts/variants_consonants_maker.sh
@@ -1,4 +1,4 @@
-# Isolate Form ---------------
+# Isolate Form -------------------------------------------------------
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni004B.isol.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0054.isol.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0053.isol.svg
@@ -8,7 +8,7 @@ open -a /Applications/Visual\ Studio\ Code.app sulive/uni0050.isol.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0048.isol.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0046.isol.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni004D.isol.svg
-# Initial Form ---------------
+# Initial Form -------------------------------------------------------
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni004B.init.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0054.init.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0053.init.svg
@@ -18,7 +18,7 @@ open -a /Applications/Visual\ Studio\ Code.app sulive/uni0050.init.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0048.init.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0046.init.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni004D.init.svg
-# Medial Form ----------------
+# Medial Form --------------------------------------------------------
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni004B.medi.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0054.medi.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0053.medi.svg
@@ -28,7 +28,7 @@ open -a /Applications/Visual\ Studio\ Code.app sulive/uni0050.medi.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0048.medi.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0046.medi.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni004D.medi.svg
-# Final Form -----------------
+# Final Form ---------------------------------------------------------
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni004B.fina.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0053.fina.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0054.fina.svg
