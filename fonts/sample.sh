b4b83574b19226f740b798b301e34cd62b5dbd8b
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-17 18:16:11 +0900

    Update sample.sh

--- fonts/sample.sh
+++ fonts/sample.sh
@@ -1,4 +1,4 @@
-open -a /Applications/Visual\ Studio\ Code.app sulive.fina.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive.init.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive.fina.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive.svg
