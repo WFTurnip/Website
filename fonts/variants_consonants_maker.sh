eacf9dad88df912ea6e32eaf9a3ccd068ff118e4
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-18 07:30:03 +0900

    Update variants_consonants_maker.sh

--- fonts/variants_consonants_maker.sh
+++ fonts/variants_consonants_maker.sh
@@ -9,32 +9,32 @@ cp sulive/uni0048.svg sulive/uni0048.isol.svg
 cp sulive/uni0046.svg sulive/uni0046.isol.svg
 cp sulive/uni004D.svg sulive/uni004D.isol.svg
 # Initial Form -------------------------------------------------------
-cat sulive.init.svg >>sulive/uni004B.init.svg
-cat sulive.init.svg >>sulive/uni0054.init.svg
-cat sulive.init.svg >>sulive/uni0053.init.svg
-cat sulive.init.svg >>sulive/uni0051.init.svg
-cat sulive.init.svg >>sulive/uni0052.init.svg
-cat sulive.init.svg >>sulive/uni0050.init.svg
-cat sulive.init.svg >>sulive/uni0048.init.svg
-cat sulive.init.svg >>sulive/uni0046.init.svg
-cat sulive.init.svg >>sulive/uni004D.init.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni004B.init.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0054.init.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0053.init.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0051.init.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0052.init.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0050.init.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0048.init.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0046.init.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni004D.init.svg
 # Medial Form --------------------------------------------------------
-cat sulive.medi.svg >>sulive/uni004B.medi.svg
-cat sulive.medi.svg >>sulive/uni0054.medi.svg
-cat sulive.medi.svg >>sulive/uni0053.medi.svg
-cat sulive.medi.svg >>sulive/uni0051.medi.svg
-cat sulive.medi.svg >>sulive/uni0052.medi.svg
-cat sulive.medi.svg >>sulive/uni0050.medi.svg
-cat sulive.medi.svg >>sulive/uni0048.medi.svg
-cat sulive.medi.svg >>sulive/uni0046.medi.svg
-cat sulive.medi.svg >>sulive/uni004D.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni004B.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0054.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0053.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0051.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0052.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0050.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0048.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0046.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni004D.medi.svg
 # Final Form ---------------------------------------------------------
-cat sulive.fina.svg >>sulive/uni004B.fina.svg
-cat sulive.fina.svg >>sulive/uni0053.fina.svg
-cat sulive.fina.svg >>sulive/uni0054.fina.svg
-cat sulive.fina.svg >>sulive/uni0051.fina.svg
-cat sulive.fina.svg >>sulive/uni0052.fina.svg
-cat sulive.fina.svg >>sulive/uni0050.fina.svg
-cat sulive.fina.svg >>sulive/uni0048.fina.svg
-cat sulive.fina.svg >>sulive/uni0046.fina.svg
-cat sulive.fina.svg >>sulive/uni004D.fina.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni004B.fina.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0053.fina.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0054.fina.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0051.fina.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0052.fina.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0050.fina.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0048.fina.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni0046.fina.svg
+open -a /Applications/Visual\ Studio\ Code.app sulive/uni004D.fina.svg
