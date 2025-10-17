cbc04d86319d36bbcca55d6f274933f3ac08f5c2
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-17 11:50:35 +0900

    update

--- fonts/consonants_maker.sh
+++ fonts/consonants_maker.sh
@@ -1,3 +1,14 @@
+cat sulive.svg >> sulive/uni004b.svg
+cat sulive.svg >> sulive/uni0054.svg
+cat sulive.svg >> sulive/uni0053.svg
+cat sulive.svg >> sulive/uni0051.svg
+cat sulive.svg >> sulive/uni0052.svg
+cat sulive.svg >> sulive/uni0050.svg
+cat sulive.svg >> sulive/uni0048.svg
+cat sulive.svg >> sulive/uni0046.svg
+cat sulive.svg >> sulive/uni004d.svg
+
+open -a /Applications/Visual\ Studio\ Code.app sulive.svg 
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni004b.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0054.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0053.svg
@@ -9,11 +20,11 @@ open -a /Applications/Visual\ Studio\ Code.app sulive/uni0046.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni004d.svg
 
 inkscape sulive/uni004b.svg
-inkscape sulive/uni0054.svg
-inkscape sulive/uni0053.svg
-inkscape sulive/uni0051.svg
-inkscape sulive/uni0052.svg
-inkscape sulive/uni0050.svg
-inkscape sulive/uni0048.svg
-inkscape sulive/uni0046.svg
-inkscape sulive/uni004d.svg
+# inkscape sulive/uni0054.svg
+# inkscape sulive/uni0053.svg
+# inkscape sulive/uni0051.svg
+# inkscape sulive/uni0052.svg
+# inkscape sulive/uni0050.svg
+# inkscape sulive/uni0048.svg
+# inkscape sulive/uni0046.svg
+# inkscape sulive/uni004d.svg
