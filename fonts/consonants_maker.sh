19e91151d543f8e509a915e204be539e37d20d58
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-17 12:52:05 +0900

    update

--- fonts/consonants_maker.sh
+++ fonts/consonants_maker.sh
@@ -1,14 +1,3 @@
-cat sulive.svg >> sulive/uni004b.svg
-cat sulive.svg >> sulive/uni0054.svg
-cat sulive.svg >> sulive/uni0053.svg
-cat sulive.svg >> sulive/uni0051.svg
-cat sulive.svg >> sulive/uni0052.svg
-cat sulive.svg >> sulive/uni0050.svg
-cat sulive.svg >> sulive/uni0048.svg
-cat sulive.svg >> sulive/uni0046.svg
-cat sulive.svg >> sulive/uni004d.svg
-
-open -a /Applications/Visual\ Studio\ Code.app sulive.svg 
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni004b.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0054.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0053.svg
@@ -19,7 +8,7 @@ open -a /Applications/Visual\ Studio\ Code.app sulive/uni0048.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni0046.svg
 open -a /Applications/Visual\ Studio\ Code.app sulive/uni004d.svg
 
-inkscape sulive/uni004b.svg
+# inkscape sulive/uni004b.svg
 # inkscape sulive/uni0054.svg
 # inkscape sulive/uni0053.svg
 # inkscape sulive/uni0051.svg
