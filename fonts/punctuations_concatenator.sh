d45ff1217f6bfab733dbcce4ec960e2996b836c7
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-19 13:31:11 +0900

    update

--- fonts/punctuations_concatenator.sh
+++ fonts/punctuations_concatenator.sh
@@ -1,8 +1,8 @@
 # cat sulive/uni003c.svg sulive/uni003e.svg >sulive/uni003d.svg
-# cat sulive/uni005C.svg sulive/uni002E.svg >sulive/uni003A.svg
-# cat sulive/uni005C.svg sulive/uni002C.svg >sulive/uni003B.svg
+cat sulive/uni005C.svg sulive/uni002E.svg >sulive/uni003A.svg
+cat sulive/uni005C.svg sulive/uni002C.svg >sulive/uni003B.svg
 # cat sulive/uni0051.svg sulive/uni003e.svg sulive/uni003f.svg >sulive/uni0026.svg
-# cat sulive/uni0035.svg sulive/uni005c.svg >sulive/uni0040.svg
+cat sulive/uni0035.svg sulive/uni005c.svg >sulive/uni0040.svg
 # cat sulive/uni0051.svg sulive/uni0050.svg >sulive/uni005f.svg
 # cat sulive/uni0051.svg sulive/uni0050.svg >sulive/uni005e.svg
 # cat sulive/uni0046.svg sulive/uni0054.svg >sulive/uni007e.svg
