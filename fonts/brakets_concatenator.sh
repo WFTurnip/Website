a1cb61bae554dda46ff67070d6efbe3c68387964
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-21 20:26:05 +0900

    update

--- fonts/brakets_concatenator.sh
+++ fonts/brakets_concatenator.sh
@@ -16,12 +16,3 @@ cp $fontname/uni0029.svg $fontname/uni005D.svg
 
 cat $fontname/uni0028.svg $fontname/uni0029.svg >$fontname/uni007B.svg
 cat $fontname/uni0028.svg $fontname/uni0029.svg >$fontname/uni007D.svg
-
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni003C.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni003E.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni0028.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni0029.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni005B.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni005D.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni007B.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni007D.svg
--- fonts/vowels_concatenator.sh
+++ fonts/vowels_concatenator.sh
@@ -1,2 +1,6 @@
-cat sulive/uni0049.svg sulive/uni0045.svg >sulive/uni0041.svg
-cat sulive/uni0057.svg sulive/uni0055.svg >sulive/uni004F.svg
+echo "開くフォントのディレクトリ名を入力してください。"
+ls -d */
+read fontname
+
+cat $fontname/uni0049.svg $fontname/uni0045.svg >$fontname/uni0041.svg
+cat $fontname/uni0057.svg $fontname/uni0055.svg >$fontname/uni004F.svg
