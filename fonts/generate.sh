8b0985447658fbe6b347ac9570d2dacdc5798fd8
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-22 12:16:20 +0900

    Update generate.sh

--- fonts/generate.sh
+++ fonts/generate.sh
@@ -1,3 +1,7 @@
+read option
+
+case $option in
+each)
 echo "生成するフォントのファイル名を入力してください。"
 ls -d *.sfd
 read fontname
@@ -5,3 +9,14 @@ read fontname
 fontforge -script generate.ff $fontname
 
 echo "フォント${fontname}の生成が完了しました。"
+;;
+all)
+for fontfile in *.sfd; do
+    fontforge -script generate.ff "$fontfile"
+    echo "フォント${fontfile}の生成が完了しました。"
+done
+;;
+*)
+echo "無効なオプションです。eachまたはallを入力してください。"
+;;
+esac
