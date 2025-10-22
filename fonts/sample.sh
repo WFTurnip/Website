80db4638959517c03619ba6b154efe01d77e37b2
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-22 15:22:45 +0900

    Update sample.sh

--- fonts/sample.sh
+++ fonts/sample.sh
@@ -2,6 +2,7 @@ fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive
 
 for fontname in "${fontname[@]}"; do
     for svg in ${fontname}/*.svg; do
-
+        # Process each SVG file
+        echo "Processing $svg"
     done
 done
