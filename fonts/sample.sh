b3be271b4b52505089e601345a5cd06ff6856d30
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-22 12:53:20 +0900

    Update sample.sh

--- fonts/sample.sh
+++ fonts/sample.sh
@@ -1,8 +1,8 @@
 fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
 
 for fontname in "${fontname[@]}"; do
-echo "" > ${fontname}.svg
-            for svg in ${fontname}/*svg; do
+    echo "" > ${fontname}.svg
+    for svg in ${fontname}/*.svg; do
         cat $svg >> ${fontname}.svg
     done
 done
