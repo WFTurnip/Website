6c76019071e14c8d8090260bac1bc092e572ab13
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-22 14:13:33 +0900

    update

--- fonts/sample.sh
+++ fonts/sample.sh
@@ -1,8 +1,6 @@
 fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
 
 for fontname in "${fontname[@]}"; do
-    echo "" > ${fontname}.svg
-    for svg in ${fontname}/*.svg; do
-        cat $svg >> ${fontname}.svg
-    done
+    inkscape ${fontname}.svg --export-plain-svg=${fontname}.svg
+    inkscape --vacuum-defs ${fontname}.svg --export-plain-svg=${fontname}.svg
 done
