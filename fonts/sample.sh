8d1721bcb63150603671e18ee887b65244b6f892
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-23 20:18:43 +0900

    update

--- fonts/sample.sh
+++ fonts/sample.sh
@@ -1,6 +1,6 @@
 fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
 
 for fontname in "${fontname[@]}"; do
-    cp $fontname/uni002c.svg $fontname/uni0060.svg
-    inkscape $fontname/uni0060.svg
+    # cat $fontname/uni002f.svg $fontname/uni0030.svg > $fontname/uni0025.svg
+    inkscape $fontname/uni0025.svg
 done
