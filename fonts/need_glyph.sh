322552c0c0affcfe42f5ee4265ef40ad595fb94f
WFTurnip <tomohiro.kabura@icloud.com>
2025-10-29 20:30:14 +0900

    update

--- fonts/need_glyph.sh
+++ fonts/need_glyph.sh
@@ -1,12 +1,5 @@
-fontnames=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
+fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
 
-for fontname in "${fontnames[@]}"; do
-    cd "$fontname" || continue
+for fontname in "${fontname[@]}"; do
 
-    for f in *.svg; do
-        inkscape "$f" \
-          --actions="select-all;object-to-path;transform-apply;export-filename=$f;export-plain-svg;quit"
-    done
-
-    cd - >/dev/null
 done
