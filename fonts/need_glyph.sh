7ab2783c78b0b8c541416266240f0893b782b99e
WFTurnip <tomohiro.kabura@icloud.com>
2025-11-02 20:14:32 +0900

    update

--- fonts/need_glyph.sh
+++ fonts/need_glyph.sh
@@ -1,5 +1,7 @@
 fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
 
 for fontname in "${fontname[@]}"; do
-    cp $fontname/uni25cc.svg $fontname/uni00b6.svg
+    cat $fontname/uni0046.svg $fontname/uni0054.svg > $fontname/tmp.svg
+    open -a /Applications/Visual\ Studio\ Code.app/ $fontname/tmp.svg
+    # inkscape $fontname/tmp.svg
 done
