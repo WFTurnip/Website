e4bdc0e31719be39a822a5e011862d7a0f1f0d7d
WFTurnip <tomohiro.kabura@icloud.com>
2025-11-03 21:26:44 +0900

    update

--- fonts/consonants_maker.sh
+++ fonts/consonants_maker.sh
@@ -2,12 +2,12 @@ echo "開くフォントのディレクトリ名を入力してください。"
 ls -d */
 read fontname
 
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni004b.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni0054.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni0053.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni0051.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni0052.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni0050.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni0048.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni0046.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni004d.svg
+touch $fontname/uni004b.svg
+touch $fontname/uni0054.svg
+touch $fontname/uni0053.svg
+touch $fontname/uni0051.svg
+touch $fontname/uni0052.svg
+touch $fontname/uni0050.svg
+touch $fontname/uni0048.svg
+touch $fontname/uni0046.svg
+touch $fontname/uni004d.svg
