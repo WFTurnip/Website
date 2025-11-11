3829b43ac3a05422e0adbb143b5521257a739b4e
WFTurnip <tomohiro.kabura@icloud.com>
2025-11-11 13:42:17 +0900

    update

--- dictionary/generate.sh
+++ dictionary/generate.sh
@@ -4,10 +4,10 @@ npm install js-beautify
 
 npm install json-beautify
 
-node make_html.js
+# node make_html.js
 
-node make_json.js
+# node make_json.js
 
-node make_favicon.js
+# node make_favicon.js
 
 node make_figure.js
