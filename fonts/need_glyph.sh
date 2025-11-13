8d3bf2bf97e852f63ebf01df20a89b110fce51fa
WFTurnip <tomohiro.kabura@icloud.com>
2025-11-14 02:11:43 +0900

    Update need_glyph.sh

--- fonts/need_glyph.sh
+++ fonts/need_glyph.sh
@@ -1,6 +1,5 @@
 fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
 
 for fontname in "${fontname[@]}"; do
-    inkscape $fontname/uni0041.svg
-    inkscape $fontname/uni004F.svg
+    inkscape $fontname/uni0052.svg
 done
