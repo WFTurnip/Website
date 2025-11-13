a25d5245a3c7611c2f334b801febc251ad2d7dfb
WFTurnip <tomohiro.kabura@icloud.com>
2025-11-13 21:09:22 +0900

    update

--- fonts/vowels_maker.sh
+++ fonts/vowels_maker.sh
@@ -2,9 +2,9 @@ echo "開くフォントのディレクトリ名を入力してください。"
 ls -d */
 read fontname
 
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni0041.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni0045.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni0049.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni004F.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni0055.svg
-open -a /Applications/Visual\ Studio\ Code.app $fontname/uni0057.svg
+inkscape $fontname/uni0041.svg
+inkscape $fontname/uni0045.svg
+inkscape $fontname/uni0049.svg
+inkscape $fontname/uni004F.svg
+inkscape $fontname/uni0055.svg
+inkscape $fontname/uni0057.svg
