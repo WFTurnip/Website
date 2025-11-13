d86be1ecf9a0d31fd263743e31f61c641fba2496
WFTurnip <tomohiro.kabura@icloud.com>
2025-11-13 14:46:44 +0900

    update

--- fonts/sample.sh
+++
@@ -1,5 +0,0 @@
-inkscape ../fonts/xesada/uni002e.svg
-inkscape ../fonts/xesada/uni003a.svg
-inkscape ../roundterm/basic_latin/uni005d.svg
-inkscape ../roundterm/basic_latin/uni007d.svg
-inkscape ../roundterm/basic_latin/uni0034.svg
