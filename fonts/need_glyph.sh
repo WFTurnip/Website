b9e1bb1dc02dac4456f7f0de18a396d2028e913f
WFTurnip <tomohiro.kabura@icloud.com>
2025-11-14 21:37:39 +0900

    update

--- fonts/need_glyph.sh
+++ fonts/need_glyph.sh
@@ -1,5 +1,5 @@
 fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
 
 for fontname in "${fontname[@]}"; do
-    cat $fontname/uni005c.svg $fontname/uni0030.svg >$fontname/uni00b6.svg
+    cat $fontname/uni007c.svg $fontname/uni0030.svg >$fontname/uni00b6.svg
 done
