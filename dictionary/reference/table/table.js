76df7735b4b61bab482a023baf0a2dea4eb776ef
WFTurnip <tomohiro.kabura@icloud.com>
2025-11-24 14:54:30 +0900

    update

--- dictionary/reference/table/table.js
+++ dictionary/reference/table/table.js
@@ -24,11 +24,11 @@ const upperTangueConsonantsConceptArray = ["剥離", "癒着", "乖離", "同一
 const middleTangueConsonantsConceptArray = ["空白", "物質", "過去", "未来", "鎮静", "高揚"];
 const lowerTangueConsonantsConceptArray = ["受動", "能動", "創造", "破壊", "流動", "固定"];
 
-function generateConsonantsModelTable() { }
+function generateConsonantsModelTable(captionName) { }
 
-function generateVowelsModelTable() { }
+function generateVowelsModelTable(captionName) { }
 
-function generateConjunctModelTable() { }
+function generateConjunctModelTable(captionName) { }
 
 function generateConsonantsPronunciationTable() {
     let table = document.createElement("table");
