7b83e733179eb828aad78702d6b05e984564b733
WFTurnip <tomohiro.kabura@icloud.com>
2025-11-30 14:50:59 +0900

    update

--- latex_dictionary/compile_tex.sh
+++ latex_dictionary/compile_tex.sh
@@ -16,3 +16,15 @@ uplatex reference.tex
 uplatex reference.tex
 
 dvipdfmx reference.dvi
+
+echo "生成ファイル一覧"
+ls *pdf 2>/dev/null || echo "生成ファイルなし。"
+echo "画像を開きますか？"
+echo "yes / no"
+read -p ">>> " check
+if [ "$check" = "yes" ]; then
+    open *pdf
+    echo "画像を開きました。"
+elif [ "$check" = "no" ]; then
+    echo "画像を開きません。"
+fi
