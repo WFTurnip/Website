42e9bb0ca15df4ffe754834779522cc6cc606625
WFTurnip <tomohiro.kabura@icloud.com>
2025-12-15 14:59:18 +0900

    update

--- latex_dictionary/compile_latex.sh
+++ latex_dictionary/compile_latex.sh
@@ -7,6 +7,27 @@ touch *
 
 mpost emblem.mp
 
+mpost adjective_conjugation_figure.mp
+mpost adverb_conjugation_figure.mp
+mpost article_conjugation_figure.mp
+mpost circumfix_consonants_figure.mp
+mpost conjunct_case_figure.mp
+mpost consonant_concept_figure.mp
+mpost consonants_figure.mp
+mpost first_case_figure.mp
+mpost ltr_figure.mp
+mpost noun_conjugation_figure.mp
+mpost part_of_speach_figure.mp
+mpost particle_conjugation_figure.mp
+mpost prefix_case_figure.mp
+mpost prefix_consonants_figure.mp
+mpost rtl_figure.mp
+mpost second_case_figure.mp
+mpost suffix_case_figure.mp
+mpost suffix_consonants_figure.mp
+mpost verb_conjugation_figure.mp
+mpost vowels_figure.mp
+
 uplatex reference.tex
 
 # upmendex reference.idx
