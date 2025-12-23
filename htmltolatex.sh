7fb139f50df347b095686625ffd41d0b6b2ccb66
WFTurnip <tomohiro.kabura@icloud.com>
2025-12-24 00:22:03 +0900

    Update htmltolatex.sh

--- htmltolatex.sh
+++ htmltolatex.sh
@@ -1,3 +1,3 @@
 echo "HTMLをLaTeXに変換します。"
-pandoc -f html -t latex -s -o latex_dictionary/reference.tex dictionary/reference/reference.html
+pandoc -f html -t latex -s -o pdf_dictionary/reference.tex dictionary/reference/reference.html
 echo "HTMLをLaTeXに変換しました。"
