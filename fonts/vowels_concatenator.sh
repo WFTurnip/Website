d6c082c386fbca53f38fe679322dcb12e2fe2ec1
WFTurnip <tomohiro.kabura@icloud.com>
2026-01-07 04:13:18 +0900

    update

--- fonts/vowels_concatenator.sh
+++ fonts/vowels_concatenator.sh
@@ -5,5 +5,5 @@ read fontname
 cat $fontname/uni0049.svg $fontname/uni0045.svg >$fontname/uni0041.svg
 cat $fontname/uni0057.svg $fontname/uni0055.svg >$fontname/uni004F.svg
 
-sed -i '' -E "s#</svg><svg width=\"[0-9]{3}\" height=\"[0-9]{3}\" xmlns=\"http://www.w3.org/2000/svg\">##g" "$fontname/uni0041.svg"
-sed -i '' -E "s#</svg><svg width=\"[0-9]{3}\" height=\"[0-9]{3}\" xmlns=\"http://www.w3.org/2000/svg\">##g" "$fontname/uni004F.svg"
+sed -i '' -E "s#[[:space:]]*</svg>[[:space:]]*<svg width=\"[0-9]{3}\" height=\"[0-9]{3}\" xmlns=\"http://www.w3.org/2000/svg\">[[:space:]]*##g" "$fontname/uni0041.svg"
+sed -i '' -E "s#[[:space:]]*</svg>[[:space:]]*<svg width=\"[0-9]{3}\" height=\"[0-9]{3}\" xmlns=\"http://www.w3.org/2000/svg\">[[:space:]]*##g" "$fontname/uni004F.svg"
