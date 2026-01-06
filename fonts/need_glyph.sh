9d66351a4923391b5eb7615ebd6081c9d14057ef
WFTurnip <tomohiro.kabura@icloud.com>
2026-01-07 03:18:02 +0900

    update

--- fonts/need_glyph.sh
+++ fonts/need_glyph.sh
@@ -1,7 +1,7 @@
 fontname=("kodito" "lekuta" "lozegw" "silwki" "xavani" "xesada" "xidili")
        
 for fontname in "${fontname[@]}"; do
-    # cat $fontname/uni0049.svg $fontname/uni0045.svg $fontname/uni0057.svg $fontname/uni0055.svg $fontname/uni0052.svg $fontname/uni007c.svg $fontname/uni0030.svg > ${fontname}.svg
-    # sed -i '' -E "s#</svg><svg width=\"[0-9]{3}\" height=\"[0-9]{3}\" xmlns=\"http://www.w3.org/2000/svg\">##g" "${fontname}.svg"
+    cat $fontname/uni0049.svg $fontname/uni0045.svg $fontname/uni0057.svg $fontname/uni0055.svg $fontname/uni0052.svg $fontname/uni007c.svg $fontname/uni0030.svg > ${fontname}.svg
+    sed -i '' -E "s#</svg><svg width=\"[0-9]{3}\" height=\"[0-9]{3}\" xmlns=\"http://www.w3.org/2000/svg\">##g" "${fontname}.svg"
     inkscape ${fontname}.svg
 done
