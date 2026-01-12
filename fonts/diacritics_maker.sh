6894a7fa84481b3183d9aa5034e790e48b0c32a0
WFTurnip <tomohiro.kabura@icloud.com>
2026-01-12 17:02:33 +0900

    update

--- fonts/diacritics_maker.sh
+++ fonts/diacritics_maker.sh
@@ -1,15 +1,17 @@
 fontname=("kodito" "lekuta" "lozegw" "silwki" "xavani" "xesada" "xidili")
 
 for fontname in "${fontname[@]}"; do
-    # cp ${fontname}.svg ${fontname}/uni0307.svg
-    # cp ${fontname}.svg ${fontname}/uni0323.svg
+    # cp ${fontname}/uni0308.svg ${fontname}/uni0300.svg
+    # cp ${fontname}/uni0324.svg ${fontname}/uni0316.svg
 
-    # cp ${fontname}/uni0307.svg ${fontname}/uni0308.svg
-    # cp ${fontname}/uni0323.svg ${fontname}/uni0324.svg
-    
     # inkscape ${fontname}/uni0307.svg
     # inkscape ${fontname}/uni0323.svg
-
     # inkscape ${fontname}/uni0308.svg
     # inkscape ${fontname}/uni0324.svg
+
+    inkscape ${fontname}/uni0301.svg
+    # inkscape ${fontname}/uni0317.svg
+
+    # inkscape ${fontname}/uni0300.svg
+    # inkscape ${fontname}/uni0316.svg
 done
