3da65eb3fddef6286e95440a8f3d60fecc65ab04
WFTurnip <tomohiro.kabura@icloud.com>
2026-01-12 12:43:38 +0900

    update

--- fonts/diacritics_maker.sh
+++ fonts/diacritics_maker.sh
@@ -4,7 +4,12 @@ for fontname in "${fontname[@]}"; do
     # cp ${fontname}.svg ${fontname}/uni0307.svg
     # cp ${fontname}.svg ${fontname}/uni0323.svg
 
-    rm ${fontname}/uni02d9.svg
+    # cp ${fontname}/uni0307.svg ${fontname}/uni0308.svg
+    # cp ${fontname}/uni0323.svg ${fontname}/uni0324.svg
+    
     # inkscape ${fontname}/uni0307.svg
     # inkscape ${fontname}/uni0323.svg
+
+    # inkscape ${fontname}/uni0308.svg
+    # inkscape ${fontname}/uni0324.svg
 done
