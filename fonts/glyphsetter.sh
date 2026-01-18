d67b7f4986c135fefccd60f1deb2092c1cf05cc6
WFTurnip <tomohiro.kabura@icloud.com>
2026-01-19 01:18:24 +0900

    update

---
+++ fonts/glyphsetter.sh
@@ -0,0 +1,5 @@
+fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
+
+for fontname in "${fontname[@]}"; do
+    fontforge -script ${fontname}.ff ${fontname}.sfd
+done
