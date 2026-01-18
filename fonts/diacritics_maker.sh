023fb0211401d13232e03681ba38a25601e70576
WFTurnip <tomohiro.kabura@icloud.com>
2026-01-18 13:52:05 +0900

    update

--- fonts/diacritics_maker.sh
+++ fonts/diacritics_maker.sh
@@ -1,17 +1,17 @@
 fontname=("kodito" "lekuta" "lozegw" "silwki" "xavani" "xesada" "xidili")
 
 for fontname in "${fontname[@]}"; do
-    # cp ${fontname}/uni0307.svg ${fontname}/uni030c.svg
-    # cp ${fontname}/uni0323.svg ${fontname}/uni032c.svg
-
+    # cp ${fontname}/uni030c.svg ${fontname}/uni0300.svg
+    # cp ${fontname}/uni032d.svg ${fontname}/uni0316.svg
+     
     # inkscape ${fontname}/uni0307.svg
     # inkscape ${fontname}/uni0323.svg
     # inkscape ${fontname}/uni0308.svg
     # inkscape ${fontname}/uni0324.svg
     # inkscape ${fontname}/uni0301.svg
     # inkscape ${fontname}/uni0317.svg
-    # inkscape ${fontname}/uni0300.svg
-    # inkscape ${fontname}/uni0316.svg
+    inkscape ${fontname}/uni0300.svg
+    inkscape ${fontname}/uni0316.svg
     # inkscape ${fontname}/uni0304.svg
     # inkscape ${fontname}/uni0331.svg
     # inkscape ${fontname}/uni0302.svg
--- fonts/need_glyph.sh
+++ fonts/need_glyph.sh
@@ -1,5 +1,5 @@
 fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
 
 for fontname in "${fontname[@]}"; do
-    rm ${fontname}.svg
+    inkscape ${fontname}/uni0027.svg
 done
