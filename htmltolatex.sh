9fc942e5e80bf912769b24f208b169e16fe6f444
WFTurnip <tomohiro.kabura@icloud.com>
2026-01-24 21:24:32 +0900

    update

--- htmltolatex.sh
+++ htmltolatex.sh
@@ -1,4 +1,4 @@
 echo "HTMLをLaTeXに変換します。"
-pandoc -f html -t latex -s -o pdf_dictionary/reference/reference.tex dictionary/reference/reference.html --toc
-pandoc -f html -t latex -s -o pdf_dictionary/cantillation/cantillation.tex dictionary/cantillation/cantillation.html --toc
+pandoc -f html -t latex -s -o pdf_dictionary/reference/reference.tex dictionary/reference/reference.html --toc --variable documentclass=uplatex --variable classoption=jsbook
+pandoc -f html -t latex -s -o pdf_dictionary/cantillation/cantillation.tex dictionary/cantillation/cantillation.html --toc  --variable documentclass=uplatex --variable classoption=jsbook
 echo "HTMLをLaTeXに変換しました。"
