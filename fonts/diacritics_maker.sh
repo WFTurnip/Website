8dd0cb3011d3b7e99428e3f03cb756cd88a616c8
WFTurnip <tomohiro.kabura@icloud.com>
2026-01-27 20:20:56 +0900

    update

--- fonts/diacritics_maker.sh
+++ fonts/diacritics_maker.sh
@@ -1,36 +1,35 @@
 #!/bin/zsh
 
-# fontname=("kodito" "lekuta" "lozegw" "silwki" "xavani" "xesada" "xidili")
+fontname=("kodito" "lekuta" "lozegw" "silwki" "xavani" "xesada" "xidili")
 
-# for fontname in "${fontname[@]}"; do
+for fontname in "${fontname[@]}"; do
 
-inkscape **/uni0301.svg
-# 高揚声符
-inkscape **/uni0317.svg
-# 低揚声符
-inkscape **/uni0300.svg
-# 高抑声符
-inkscape **/uni0316.svg
-# 低抑声符
-inkscape **/uni0302.svg
-# 高昇声符
-inkscape **/uni032d.svg
-# 低昇声符
-inkscape **/uni030c.svg
-# 高降声符
-inkscape **/uni032c.svg
-# 低降声符
-inkscape **/uni0304.svg
-# 高平声符
-inkscape **/uni0331.svg
-# 低平声符
-inkscape **/uni0308.svg
-# 高分声符
-inkscape **/uni0324.svg
-# 低分声符
-inkscape **/uni0307.svg
-# 高終声符
-inkscape **/uni0323.svg
-# 低終声符
-
-# done
+    touch ${fontname}/uni0301.svg
+    # 高揚声符
+    touch ${fontname}/uni0317.svg
+    # 低揚声符
+    touch ${fontname}/uni0300.svg
+    # 高抑声符
+    touch ${fontname}/uni0316.svg
+    # 低抑声符
+    touch ${fontname}/uni0302.svg
+    # 高昇声符
+    touch ${fontname}/uni032d.svg
+    # 低昇声符
+    touch ${fontname}/uni030c.svg
+    # 高降声符
+    touch ${fontname}/uni032c.svg
+    # 低降声符
+    touch ${fontname}/uni0304.svg
+    # 高平声符
+    touch ${fontname}/uni0331.svg
+    # 低平声符
+    touch ${fontname}/uni0308.svg
+    # 高分声符
+    touch ${fontname}/uni0324.svg
+    # 低分声符
+    touch ${fontname}/uni0307.svg
+    # 高終声符
+    touch ${fontname}/uni0323.svg
+    # 低終声符
+done
