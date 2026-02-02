bc6930d2f35f31695c720f79849ae4fa0f9dee02
WFTurnip <tomohiro.kabura@icloud.com>
2026-02-02 22:59:52 +0900

    update

--- creative/fonts/diacritics_maker.sh
+++ creative/fonts/diacritics_maker.sh
@@ -4,32 +4,41 @@ fontname=("kodito" "lekuta" "lozegw" "silwki" "xavani" "xesada" "xidili")
 
 for fontname in "${fontname[@]}"; do
 
-    touch ${fontname}/uni0301.svg
-    # 高揚声符
-    touch ${fontname}/uni0317.svg
-    # 低揚声符
-    touch ${fontname}/uni0300.svg
-    # 高抑声符
-    touch ${fontname}/uni0316.svg
-    # 低抑声符
-    touch ${fontname}/uni0302.svg
-    # 高昇声符
-    touch ${fontname}/uni032d.svg
-    # 低昇声符
-    touch ${fontname}/uni030c.svg
-    # 高降声符
-    touch ${fontname}/uni032c.svg
-    # 低降声符
-    touch ${fontname}/uni0304.svg
-    # 高平声符
-    touch ${fontname}/uni0331.svg
-    # 低平声符
-    touch ${fontname}/uni0308.svg
-    # 高分声符
-    touch ${fontname}/uni0324.svg
-    # 低分声符
-    touch ${fontname}/uni0307.svg
-    # 高終声符
-    touch ${fontname}/uni0323.svg
-    # 低終声符
-done
+    cp ${fontname}/uni0307.svg ${fontname}/uni0302.svg
+    cp ${fontname}/uni0323.svg ${fontname}/uni032d.svg
+    
+    if [[ $fontname == "xesada" ]]; then
+        cp ${fontname}.svg ${fontname}/uni0302.svg
+        cp ${fontname}.svg ${fontname}/uni032d.svg
+    fi
+done    
+
+touch **/uni0301.svg
+# 高揚声符
+touch **/uni0317.svg
+# 低揚声符
+touch **/uni0300.svg
+# 高抑声符
+touch **/uni0316.svg
+# 低抑声符
+inkscape **/uni0302.svg
+# 高昇声符
+inkscape **/uni032d.svg
+# 低昇声符
+touch **/uni030c.svg
+# 高降声符
+touch **/uni032c.svg
+# 低降声符
+touch **/uni0304.svg
+# 高平声符
+touch **/uni0331.svg
+# 低平声符
+touch **/uni0308.svg
+# 高分声符
+touch **/uni0324.svg
+# 低分声符
+touch **/uni0307.svg
+# 高終声符
+touch **/uni0323.svg
+# 低終声符
+
