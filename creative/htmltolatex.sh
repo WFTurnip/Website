dcc96be150d1fb4b1760e763e48aa6911fdc664d
WFTurnip <tomohiro.kabura@icloud.com>
2026-03-22 15:18:48 +0900

    update

--- creative/htmltolatex.sh
+++ creative/htmltolatex.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 echo "HTMLをLaTeXに変換します。"
-pandoc -f html -t latex -s -o pdf_dictionary/reference/reference.tex dictionary/reference/reference.html --toc --pdf-engine=latexmk --pdf-engine-opt="-r"
+pandoc -f html -t latex -s -o pdf_dictionary/grammar/grammar.tex dictionary/grammar/grammar.html --toc --pdf-engine=latexmk --pdf-engine-opt="-r" --pdf-engine-opt="./htmltolatex.latexmk"
 pandoc -f html -t latex -s -o pdf_dictionary/cantillation/cantillation.tex dictionary/cantillation/cantillation.html --toc --pdf-engine=latexmk --pdf-engine-opt="-r" --pdf-engine-opt="./htmltolatex.latexmk"
 echo "HTMLをLaTeXに変換しました。"
