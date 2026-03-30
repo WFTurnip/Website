344c3d7103e6b97bb9a48bc695e9ea0f28e64661
WFTurnip <tomohiro.kabura@icloud.com>
2026-03-30 22:09:06 +0900

    update

--- creative/fonts/punctuations_concatenator.sh
+++ creative/fonts/punctuations_concatenator.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-echo "結合するフォントのディレクトリ名を入力してください。"
-ls -d */
-read -p ">>> " fontname
+fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
 
-cat "${fontname}"/uni0044.svg "${fontname}"/uni007c.svg >"${fontname}"/uni0024.svg
-cat "${fontname}"/uni0030.svg "${fontname}"/uni002f.svg >"${fontname}"/uni0025.svg
-cat "${fontname}"/uni0051.svg "${fontname}"/uni003e.svg "${fontname}"/uni0027.svg >"${fontname}"/uni0026.svg
-cat "${fontname}"/uni002c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni003b.svg
-cat "${fontname}"/uni002e.svg "${fontname}"/uni0030.svg >"${fontname}"/uni003a.svg
-cat "${fontname}"/uni005c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni00b6.svg
+# echo "結合するフォントのディレクトリ名を入力してください。"
+# ls -d */
+# read -p ">>> " fontname
+for fontname in "${fontname[@]}"; do
+    cat "${fontname}"/uni0030.svg "${fontname}"/uni002d.svg "${fontname}"/uni002f.svg "${fontname}"/uni005c.svg >"${fontname}"/uni0024.svg
+    # cat "${fontname}"/uni0030.svg "${fontname}"/uni002f.svg >"${fontname}"/uni0025.svg
+    # cat "${fontname}"/uni0051.svg "${fontname}"/uni003e.svg "${fontname}"/uni0027.svg >"${fontname}"/uni0026.svg
+    # cat "${fontname}"/uni002c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni003b.svg
+    # cat "${fontname}"/uni002e.svg "${fontname}"/uni0030.svg >"${fontname}"/uni003a.svg
+    # cat "${fontname}"/uni005c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni00b6.svg
+    open -a /Applications/Visual\ Studio\ Code.app "${fontname}"/uni0024.svg
+done
