ab29a11084818619b6d94a88a467b3c3dc33fdba
WFTurnip <tomohiro.kabura@icloud.com>
2026-04-01 20:59:11 +0900

    update

--- creative/fonts/punctuations_concatenator.sh
+++ creative/fonts/punctuations_concatenator.sh
@@ -6,7 +6,7 @@ fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive
 # ls -d */
 # read -p ">>> " fontname
 for fontname in "${fontname[@]}"; do
-    cat "${fontname}"/uni002d.svg "${fontname}"/uni002f.svg "${fontname}"/uni005c.svg >"${fontname}"/uni0024.svg
+    cat "${fontname}"/uni0030.svg "${fontname}"/uni0052.svg "${fontname}"/uni002d.svg "${fontname}"/uni002f.svg "${fontname}"/uni005c.svg >"${fontname}".svg
     # cat "${fontname}"/uni0030.svg "${fontname}"/uni002f.svg >"${fontname}"/uni0025.svg
     # cat "${fontname}"/uni0051.svg "${fontname}"/uni003e.svg "${fontname}"/uni0027.svg >"${fontname}"/uni0026.svg
     # cat "${fontname}"/uni002c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni003b.svg
