cee8811f422668cda76ebea7b9414a8ba3edd78c
WFTurnip <tomohiro.kabura@icloud.com>
2026-04-05 15:00:07 +0900

    update

--- creative/fonts/punctuations_concatenator.sh
+++ creative/fonts/punctuations_concatenator.sh
@@ -6,12 +6,12 @@ fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive
 # ls -d */
 # read -p ">>> " fontname
 for fontname in "${fontname[@]}"; do
-    cat "${fontname}"/uni002f.svg "${fontname}"/uni005c.svg >"${fontname}".svg  
+    # cat "${fontname}".svg >>"${fontname}"/uni0024.svg
     # cp "${fontname}"/uni0030.svg "${fontname}"/uni0024.svg
     # cat "${fontname}"/uni0030.svg "${fontname}"/uni002f.svg >"${fontname}"/uni0025.svg
     # cat "${fontname}"/uni0051.svg "${fontname}"/uni003e.svg "${fontname}"/uni0027.svg >"${fontname}"/uni0026.svg
     # cat "${fontname}"/uni002c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni003b.svg
     # cat "${fontname}"/uni002e.svg "${fontname}"/uni0030.svg >"${fontname}"/uni003a.svg
     # cat "${fontname}"/uni005c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni00b6.svg
-    # open -a /Applications/Visual\ Studio\ Code.app "${fontname}"/uni0024.svg
+    open -a /Applications/Inkscape.app "${fontname}"/uni0024.svg
 done
