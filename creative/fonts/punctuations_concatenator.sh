82d6ffbc38650b0327bf92915af3a6fdcfcde0a2
WFTurnip <tomohiro.kabura@icloud.com>
2026-04-07 19:37:16 +0900

    update

--- creative/fonts/punctuations_concatenator.sh
+++ creative/fonts/punctuations_concatenator.sh
@@ -1,4 +1,4 @@
-]1;95;0c#!/bin/bash
+#!/bin/bash
 
 fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
 
@@ -6,11 +6,10 @@ fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive
 # ls -d */
 # read -p ">>> " fontname
 for fontname in "${fontname[@]}"; do
-    cat "${fontname}"/uni0030.svg >>"${fontname}"/uni0024.svg
+    cat "${fontname}"/uni002d.svg "${fontname}".svg >> "${fontname}"/uni0024.svg 
     # cat "${fontname}"/uni0030.svg "${fontname}"/uni002f.svg >"${fontname}"/uni0025.svg
     # cat "${fontname}"/uni0051.svg "${fontname}"/uni003e.svg "${fontname}"/uni0027.svg >"${fontname}"/uni0026.svg
     # cat "${fontname}"/uni002c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni003b.svg
     # cat "${fontname}"/uni002e.svg "${fontname}"/uni0030.svg >"${fontname}"/uni003a.svg
     # cat "${fontname}"/uni005c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni00b6.svg
-    open -a /Applications/Inkscape.app "${fontname}"/uni0024.svg
 done
