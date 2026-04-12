f66f4fccef9561760fd4b10e21e8cbda0299195c
WFTurnip <tomohiro.kabura@icloud.com>
2026-04-12 13:13:32 +0900

    update

--- creative/fonts/consonants_concatenator.sh
+++ creative/fonts/consonants_concatenator.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-echo "結合するフォントのディレクトリ名を入力してください。"
-ls -d */
-read -p ">>> " fontname
+fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
 
-cat "${fontname}"/uni004b.svg "${fontname}"/uni005c.svg >"${fontname}"/uni0047.svg
-cat "${fontname}"/uni0054.svg "${fontname}"/uni005c.svg >"${fontname}"/uni0044.svg
-cat "${fontname}"/uni0053.svg "${fontname}"/uni005c.svg >"${fontname}"/uni005a.svg
-cat "${fontname}"/uni0051.svg "${fontname}"/uni005c.svg >"${fontname}"/uni0043.svg
-cat "${fontname}"/uni0052.svg "${fontname}"/uni005c.svg >"${fontname}"/uni004c.svg
-cat "${fontname}"/uni0050.svg "${fontname}"/uni005c.svg >"${fontname}"/uni0042.svg
-cat "${fontname}"/uni0048.svg "${fontname}"/uni005c.svg >"${fontname}"/uni0058.svg
-cat "${fontname}"/uni0046.svg "${fontname}"/uni005c.svg >"${fontname}"/uni0056.svg
-cat "${fontname}"/uni004d.svg "${fontname}"/uni005c.svg >"${fontname}"/uni004e.svg
-cat "${fontname}"/uni0053.svg "${fontname}"/uni005c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni004a.svg
-cat "${fontname}"/uni0051.svg "${fontname}"/uni005c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni0059.svg
+for fontname in "${fontname[@]}"; do
+    cat "${fontname}"/uni004b.svg "${fontname}"/uni005c.svg >"${fontname}"/uni0047.svg
+    cat "${fontname}"/uni0054.svg "${fontname}"/uni005c.svg >"${fontname}"/uni0044.svg
+    cat "${fontname}"/uni0053.svg "${fontname}"/uni005c.svg >"${fontname}"/uni005a.svg
+    cat "${fontname}"/uni0051.svg "${fontname}"/uni005c.svg >"${fontname}"/uni0043.svg
+    cat "${fontname}"/uni0052.svg "${fontname}"/uni005c.svg >"${fontname}"/uni004c.svg
+    cat "${fontname}"/uni0050.svg "${fontname}"/uni005c.svg >"${fontname}"/uni0042.svg
+    cat "${fontname}"/uni0048.svg "${fontname}"/uni005c.svg >"${fontname}"/uni0058.svg
+    cat "${fontname}"/uni0046.svg "${fontname}"/uni005c.svg >"${fontname}"/uni0056.svg
+    cat "${fontname}"/uni004d.svg "${fontname}"/uni005c.svg >"${fontname}"/uni004e.svg
+    cat "${fontname}"/uni0053.svg "${fontname}"/uni005c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni004a.svg
+    cat "${fontname}"/uni0051.svg "${fontname}"/uni005c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni0059.svg
+    if [ "${fontname}" = "piswpi" ]; then
+        cat "${fontname}"/uni0053.svg "${fontname}"/uni0027.svg "${fontname}"/uni0030.svg >"${fontname}"/uni004a.svg
+        cat "${fontname}"/uni0051.svg "${fontname}"/uni0027.svg "${fontname}"/uni0030.svg >"${fontname}"/uni0059.svg
+    fi
+done
--- creative/fonts/punctuations_concatenator.sh
+++ creative/fonts/punctuations_concatenator.sh
@@ -2,14 +2,10 @@
 
 fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
 
-# echo "結合するフォントのディレクトリ名を入力してください。"
-# ls -d */
-# read -p ">>> " fontname
 for fontname in "${fontname[@]}"; do
-    cat "${fontname}"/uni002d.svg "${fontname}".svg >> "${fontname}"/uni0024.svg 
-    # cat "${fontname}"/uni0030.svg "${fontname}"/uni002f.svg >"${fontname}"/uni0025.svg
-    # cat "${fontname}"/uni0051.svg "${fontname}"/uni003e.svg "${fontname}"/uni0027.svg >"${fontname}"/uni0026.svg
-    # cat "${fontname}"/uni002c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni003b.svg
-    # cat "${fontname}"/uni002e.svg "${fontname}"/uni0030.svg >"${fontname}"/uni003a.svg
-    # cat "${fontname}"/uni005c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni00b6.svg
+    cat "${fontname}"/uni0030.svg "${fontname}"/uni002f.svg >"${fontname}"/uni0025.svg
+    cat "${fontname}"/uni0051.svg "${fontname}"/uni003e.svg "${fontname}"/uni0027.svg >"${fontname}"/uni0026.svg
+    cat "${fontname}"/uni002c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni003b.svg
+    cat "${fontname}"/uni002e.svg "${fontname}"/uni0030.svg >"${fontname}"/uni003a.svg
+    cat "${fontname}"/uni005c.svg "${fontname}"/uni0030.svg >"${fontname}"/uni00b6.svg
 done
