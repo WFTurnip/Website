9e582779bf1ee4f6227d776f227a92f568ea9825
WFTurnip <tomohiro.kabura@icloud.com>
2026-04-19 21:43:08 +0900

    Update kiita_declaration.js

--- creative/fonts/kiita_declaration.js
+++ creative/fonts/kiita_declaration.js
@@ -1,33 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
-    function changeFont(number) {
-        if (document.getElementById) {
-            let main = document.getElementsByTagName("main")[0];
-            switch (number) {
-                case 0: main.className = "kodito";
-                    break;
-                case 1: main.className = "lekuta";
-                    break;
-                case 2: main.className = "lozegw";
-                    break;
-                case 3: main.className = "makina";
-                    break;
-                case 4: main.className = "piswpi";
-                    break;
-                case 5: main.className = "polwgo";
-                    break;
-                case 6: main.className = "silwki";
-                    break;
-                case 7: main.className = "sulive";
-                    break;
-                case 8: main.className = "xavani";
-                    break;
-                case 9: main.className = "xesada";
-                    break;
-                case 10: main.className = "xidili";
-                    break;
-                case 11: main.className = "zosokw";
-                    break;
-            }
+    const main = document.getElementsByTagName("main")[0];
+    document.querySelector("form").addEventListener("click", (e) => {
+        if (e.target.matches("input[type='button']")) {
+            main.className = e.target.value;
         }
-    }
+    });
 });
